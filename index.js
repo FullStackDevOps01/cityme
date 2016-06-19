4bd7a7a0fe2ca02a90ce3a2a0512d8036d2e5e8a
Tien Nguyen <nguyentien8x@gmail.com>
2016-06-19 21:41:49 +0700

    DevOps - buổi 3

--- index.js
+++ index.js
@@ -2,13 +2,31 @@ var express = require('express');
 var app = express();
 var logger = require('./helpers/logger');
 var config = require('config');
+var bodyParser = require('body-parser');
+var yaml = require('js-yaml');
+var fs = require('fs');
+
+// body parse
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// add-on swagger-editor
+app.use('/swagger', express.static('./node_modules/swagger-editor'));
+app.use('/', express.static('./docs'));
+app.get('/docs', function(req, res){
+    var docs = yaml.safeLoad(fs.readFileSync('./docs/swagger.yml', 'utf8'));
+    res.send(JSON.stringify(docs));
+});
+
+// import routers
+app.use(require('./apis'));
+
+app.get('/hello', function(req, res) {
+	res.send(JSON.stringify({'Hello': 'world'}));
+});
 
 var server = app.listen(config.get('server.port'), config.get('server.host'), function() {
 	var host = server.address().address;
 	var port = server.address().port;
 	logger.info('Server start at http://%s:%s', config.get('server.host'), config.get('server.port'));
-});
-
-app.get('/hello', function(req, res) {
-	res.send(JSON.stringify({'Hello': 'world'}));
 });
